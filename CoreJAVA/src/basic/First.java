f1e5da1db456258b789c2a9e4cfde65f449838c7
Yogi2001 <yogitagidwani88@gmail.com>
2024-05-25 16:30:12 +0530

    first commit

---
+++ CoreJAVA/src/basic/First.java
@@ -0,0 +1,13 @@
+// There are total 3 streams
+//1. In
+//2. Out
+//3. Err
+
+package basic;
+
+public class First {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello Java");
+	}
+}
